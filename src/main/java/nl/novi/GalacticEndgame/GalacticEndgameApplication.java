2a05243084586c3d0594613182d7e17146505c20
qwyrdeer <qwyrdeer@wdotwood.com>
2026-01-29 21:07:10 +0100

    setup project, added folders & copied helper - added ignore - injected dependencies

---
+++ src/main/java/nl/novi/GalacticEndgame/GalacticEndgameApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.GalacticEndgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalacticEndgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalacticEndgameApplication.class, args);
+	}
+
+}
---
+++ src/main/java/nl/novi/GalacticEndgame/entities/UserEntity.java
@@ -0,0 +1,4 @@
+package nl.novi.GalacticEndgame.entities;
+
+public class UserEntity {
+}
